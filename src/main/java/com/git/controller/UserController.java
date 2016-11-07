41c44f1268bcf8de32239799920331f100961a19
wangquanjava <wangquanjava@163.com>
2016-11-07 17:04:38 +0800

    增加超时时间

--- src/main/java/com/git/controller/UserController.java
+++ src/main/java/com/git/controller/UserController.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.git.domain.AjaxJson;
 import com.git.domain.UserEntity;
-import com.git.exception.GlobeExceptionResolver;
 import com.git.service.UserService;
 
 @Controller
